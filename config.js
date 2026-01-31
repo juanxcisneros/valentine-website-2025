d2a274351384d7064d5bf7d2c615df04e82a89e8
juanxcisneros <juancnrs10@gmail.com>
2026-01-31 17:04:24 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dbs0umypn/video/upload/v1769899628/videoplayback_meoku5.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dbs0umypn/video/upload/v1769900607/YTDowncom_YouTube_manos-perez-cover_Media_HagpdFqqHSI_001_1080p_online-audio-converter.com_avm4te.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
